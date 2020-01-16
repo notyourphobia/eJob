3d9fa248ebfa8044fc77106f154b9cff6911191a
notyourphobia <notyourphobia@gmail.com>
2020-01-16 23:02:11 +0200

    d

---
+++ exe2/exe2/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Exe2 from './components/exe2';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+
+      <img src={logo} className="App-logo" alt="logo" />
+      <Exe2 />
+    </div>
+  );
+}
+
+export default App;
